3a42aee075039da83e75f40dd088c07d002f22ff
Seth Barden <sethbarden@gmail.com>
2017-09-04 16:11:04 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 const app = "I don't do much."
 
-
-
 function destructivelyAppendKitten(name) {
   return kittens.push(name)  
+}
+
+function destructivelyPrependKitten(name) {
+  return kittens.unshift(name)
 }
